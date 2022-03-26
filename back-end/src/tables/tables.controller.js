0969819e7461a233194dd2fa05bed3515cd6bf0d
nickrizzo76 <nrizzo414@gmail.com>
2022-03-26 18:41:57 -0400

    Passes test for nonexistent reservation for reservation_id

--- back-end/src/tables/tables.controller.js
+++ back-end/src/tables/tables.controller.js
@@ -1,5 +1,6 @@
 const service = require("./tables.service");
 const asyncErrorBoundary = require("../errors/asyncErrorBoundary");
+const reservationsService = require("../reservations/reservations.service");
 
 function bodyHasData() {
   return function (req, _res, next) {
@@ -7,53 +8,68 @@ function bodyHasData() {
     if (!data)
       next({
         status: 400,
-        message: "body"
+        message: "body",
       });
     next();
   };
 }
 
 function bodyHasReservationId() {
-    return function (req, res, next) {
-        const { reservation_id } = req.body.data;
-        if(!reservation_id) return next({
-            status: 400,
-            message: "reservation_id"
-        })
-        res.locals.reservation_id = reservation_id;
-        next();
-    }
+  return function (req, res, next) {
+    const { reservation_id } = req.body.data;
+    if (!reservation_id)
+      return next({
+        status: 400,
+        message: "reservation_id",
+      });
+    res.locals.reservation_id = reservation_id;
+    next();
+  };
 }
 
 function nameIsValid() {
-    return function (req, _res, next) {
-        const { table_name } = req.body.data;
-        if(!table_name || !table_name.length || table_name.length === 1) return next({ status: 400, message: "table_name" })
-        next();
-    }
+  return function (req, _res, next) {
+    const { table_name } = req.body.data;
+    if (!table_name || !table_name.length || table_name.length === 1)
+      return next({ status: 400, message: "table_name" });
+    next();
+  };
 }
 
 function capacityIsValid() {
-    return function (req, _res, next) {
-        const { capacity } = req.body.data;
-        if(!capacity || (typeof capacity !== 'number')) next({status: 400, message: "capacity"})
-        next();
-    }
+  return function (req, _res, next) {
+    const { capacity } = req.body.data;
+    if (!capacity || typeof capacity !== "number")
+      next({ status: 400, message: "capacity" });
+    next();
+  };
 }
 
 function tableExists() {
-    return async function(req, res, next) {
-        const { table_id } = req.params;
-        const table = await service.read(table_id)
-        if(table) {
-            res.locals.table = table;
-            return next()
-        }
-        next({
-            status: 404,
-            message: "table_id"
-        })
+  return async function (req, res, next) {
+    const { table_id } = req.params;
+    const table = await service.read(table_id);
+    if (table) {
+      res.locals.table = table;
+      return next();
     }
+    next({
+      status: 404,
+      message: "table_id",
+    });
+  };
+}
+
+function reservationIdExists() {
+  return async function (_req, res, next) {
+    const reservation = await reservationsService.read(
+      res.locals.reservation_id
+    );
+    if (!reservation)
+      return next({ status: 404, message: `${res.locals.reservation_id}` });
+    res.locals.reservation = reservation;
+    next();
+  };
 }
 
 async function create(req, res, _next) {
@@ -69,17 +85,23 @@ async function list(_req, res, _next) {
 // }
 
 async function update(_req, res, _next) {
-    res.json({ data: {} });
+  res.json({ data: {} });
 }
 
 module.exports = {
   // list: asyncErrorBoundary(list),
-  create: [bodyHasData(), nameIsValid(), capacityIsValid(), asyncErrorBoundary(create)],
+  create: [
+    bodyHasData(),
+    nameIsValid(),
+    capacityIsValid(),
+    asyncErrorBoundary(create),
+  ],
   list: asyncErrorBoundary(list),
   update: [
     bodyHasData(),
     bodyHasReservationId(),
+    asyncErrorBoundary(reservationIdExists()),
     asyncErrorBoundary(tableExists()),
-    asyncErrorBoundary(update)
-  ]
+    asyncErrorBoundary(update),
+  ],
 };
