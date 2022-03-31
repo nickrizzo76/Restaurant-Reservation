03b19aa3402b2b98c8ee25b7dbd511097db18c95
nickrizzo76 <nrizzo414@gmail.com>
2022-03-31 14:50:35 -0400

    Updates endpoint for reservation creation

--- front-end/src/utils/api.js
+++ front-end/src/utils/api.js
@@ -76,7 +76,7 @@ export async function listReservations(params, signal) {
  */
 
 export async function createReservation(reservation, signal) {
-  const url = new URL(`${API_BASE_URL}/tables`);
+  const url = new URL(`${API_BASE_URL}/reservations`);
   const options = {
     method: 'POST',
     mode: 'cors',
